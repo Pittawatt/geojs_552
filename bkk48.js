198b0136e42be8947f73312fcbfc120818b7e45c
Pittawatt <124548012+Pittawatt@users.noreply.github.com>
2023-04-24 11:14:07 +0700

    Add files via upload

---
+++ bkk48.js
@@ -0,0 +1,155 @@
+var bkk48 = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนวชิรเบญจทัศ",
+          "type": "ธรรมชาติ",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.55357245636219,
+            13.815700113566749
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หอศิลป์",
+          "type": "ศิลปะ",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.55090718519659,
+            13.838943529856493
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ดองกิ มอลล์",
+          "type": "ห้างสรรพสินค้า",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.58590299868871,
+            13.730919162084177
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนเบญจกิติ",
+          "type": "ธรรมชาติ",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.55855272752609,
+            13.730589117616475
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ไบเทค",
+          "type": "ห้างสรรพสินค้า",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.61002445636092,
+            13.669622120499056
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สยามพารากอน",
+          "type": "ห้างสรรพสินค้า",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.53495599869001,
+            13.74669511685002
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ไอคอนสยาม",
+          "type": "ห้างสรรพสินค้า",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.51042576985657,
+            13.726691350584701
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนจตุจักร",
+          "type": "ธรรมชาติ",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.55311343519281,
+            13.80352803313184
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ดรีมเวิลด์",
+          "type": "สวนสนุก",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.67488172752456,
+            13.987490373196934
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สยามพาร์คซิตี้",
+          "type": "สวนสนุก",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.69528068519759,
+            13.806178276295
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
